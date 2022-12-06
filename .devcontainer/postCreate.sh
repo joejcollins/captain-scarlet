c1be26294dde0f70f7c349976e630f3b07d8af5d
Joe J Collins <joejcollins@users.noreply.github.com>
2022-12-06 14:19:13 +0000

    Test

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,5 +4,3 @@ sudo usermod -a -G sudo rstudio
 # https://stackoverflow.com/questions/47541007/how-to-i-bypass-the-login-page-on-rstudio
 sudo bash -c "echo 'server-user=rstudio' >> /etc/rstudio/rserver.conf"
 sudo bash -c "echo 'auth-none=1' >> /etc/rstudio/rserver.conf"
-# Start the rserver with sudo otherwise it won't run for the gitpod user
-sudo rserver
