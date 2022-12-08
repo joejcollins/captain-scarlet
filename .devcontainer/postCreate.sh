82de5e52ad272fa50f227dc46f069341f2ebca22
Joe J Collins <joejcollins@users.noreply.github.com>
2022-12-08 16:43:55 +0000

    Use the rstudio user

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,9 +1,9 @@
 # Set the working directory
 sudo bash -c "echo R_LIBS_USER=$CODESPACE_VSCODE_FOLDER/../.R/library > /home/gitpod/.Renviron"
-ln -s /workspace/captain-scarlet /home/gitpod/captain-scarlet
+ln -s /workspace/captain-scarlet /home/rstudio/captain-scarlet
 # https://stackoverflow.com/questions/47541007/how-to-i-bypass-the-login-page-on-rstudio
-sudo usermod -a -G sudo vscode
-sudo bash -c "echo 'server-user=vscode' >> /etc/rstudio/rserver.conf"
+sudo usermod -a -G root rstudio
+sudo bash -c "echo 'server-user=rstudio' >> /etc/rstudio/rserver.conf"
 sudo bash -c "echo 'auth-none=1' >> /etc/rstudio/rserver.conf"
 # Restart the rserver with sudo otherwise it won't run for the local user (dunno why)
 sudo rserver
