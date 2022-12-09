7f496fbe5900e382fb98aa4a78cc3585dc24d9c5
Joe J Collins <joejcollins@users.noreply.github.com>
2022-12-09 18:39:49 +0000

    VSCode to edit R lang

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,3 @@
-# Set the working directory
-echo Renviron config
-touch /home/rstudio/.Renviron
-sudo bash -c "echo R_LIBS_USER=$CODESPACE_VSCODE_FOLDER/../.R/library > /home/rstudio/.Renviron"
-ln -s /workspaces/captain-scarlet /home/rstudio/captain-scarlet
-# Restart the rserver with sudo otherwise it won't run for the local user (dunno why)
-echo Rserver setup
-sudo rserver
-sudo pkill rserver
 # Setup the Python environment
 echo Python environment
 python3 -m venv .venv
