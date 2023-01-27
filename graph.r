418748f3d27f7ca465a073ad1b0274da09e26390
Joe Collins <joejcollins@gmail.com>
2023-01-27 21:10:02 +0000

    Co-pilot test

--- graph.r
+++ graph.r
@@ -1,2 +1,4 @@
 curve(x^2+5)
 
+# draw and sine wave curve
+curve(sin(x), from=0, to=2*pi, col="red", lwd=2)
--- model.r
+++ model.r
@@ -1,2 +1,7 @@
 numbers <- read.csv("./data.csv", stringsAsFactors=FALSE)
 
+# Filter out the numbers lower that 12
+numbers <- numbers[numbers$number > 12,]
+
+# Save the filtered numbers
+write.csv(numbers, "./filtered.csv")
