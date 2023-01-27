10fe251e0edb5063e6be4ee633726cae9dffeedc
Joe Collins <joejcollins@gmail.com>
2023-01-27 21:58:12 +0000

    Emojis

--- graph.r
+++ graph.r
@@ -1,4 +1,4 @@
 curve(x^2+5)
 
-# draw and sine wave curve
+# draw and sine wave curve  \U0001F600
 curve(sin(x), from=0, to=2*pi, col="red", lwd=2)
---
+++ shit.py
@@ -0,0 +1,10 @@
+# conditional statements with emojis
+
+is_on = True
+
+if is_on:
+    # do something end with boat emoji 🚣
+    print("👍")
+else:
+    print("👎")
+ 
