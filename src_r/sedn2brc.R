29cc65e297cde1abe5c93f32e283e22f052c88ee
Joe Collins <joejcollins@gmail.com>
2023-03-24 22:57:34 +0000

    Pass the dataframe

--- src_r/sedn2brc.R
+++ src_r/sedn2brc.R
@@ -4,21 +4,23 @@ rename_columns <- function(df) {
   new_name_map <- c("Common.Name" = "Common name",
                  "Site" = "Site name")
   names(df) <- new_name_map[names(df)]
+  return(df)
 }
 
 
 # Remove columns from a dataframe
-remove_columns <- function(df) {
+remove_columns <- function(df_with_all_columns) {
   cols_to_remove <- c("Original.name", "Sort.order")
-  df <- df[, !(names(df) %in% cols_to_remove)]
-  return df
+  df_with_less_columns <- df_with_all_columns[, 
+    !(names(df_with_all_columns) %in% cols_to_remove)]
+  return(df_with_less_columns)
 }
 
 
 ################################################################################
 # Main script
 data_path <- "./data/raw"
-all_files <- list.files(data_path, pattern = "*.csv", full.names = TRUE)
+all_files <- list.files(data_path, pattern = "*1.csv", full.names = TRUE)
 
 # Create a single dataframe from the CSV files
 dataframes <- list()
@@ -29,7 +31,7 @@ for (filename in all_files) {
 merged_df <- do.call(rbind, dataframes)
 
 # Tidy up the columns.
-rename_columns(merged_df)
+merged_df <- rename_columns(merged_df)
 merged_df <- remove_columns(merged_df)
 
 write.csv(merged_df, file = "./data/processed/VC40.csv", row.names = FALSE)
