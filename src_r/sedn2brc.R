d84415d9de1e218b4c3625f4fee232714fce1c78
Joe Collins <joejcollins@gmail.com>
2023-03-24 23:21:21 +0000

    Organized tests

--- src_r/sedn2brc.R
+++ src_r/sedn2brc.R
@@ -1,14 +1,35 @@
-
-# Rename the SEDN columns to suit BRC
-rename_columns <- function(df) {
-  new_name_map <- c("Common.Name" = "Common name",
-                 "Site" = "Site name")
-  names(df) <- new_name_map[names(df)]
-  return(df)
+# Merge the split data files.
+merge_csv_files <- function(data_path) {
+  all_files <- list.files(data_path, pattern = "*1.csv", full.names = TRUE)
+  # Create a single data frame from the CSV files
+  data_frames <- list()
+  for (filename in all_files) {
+    df <- read.csv(filename)
+    data_frames <- append(data_frames, list(df))
+  }
+  merged_df <- do.call(rbind, data_frames)
+  return(merged_df)
 }
 
 
-# Remove columns from a dataframe
+# Rename the SEDN columns to suit BRC.
+rename_columns <- function(df_to_rename) {
+  rename_map <- c("Common.Name" = "Common name",
+                  "Site" = "Site name")
+  for (i in 1:length(rename_map)) {
+    col_name <- names(rename_map[i])
+    new_col_name <- rename_map[[i]]
+    if (col_name %in% names(df_to_rename)) {
+      names(df_to_rename)[
+        which(names(df_to_rename) == col_name)
+        ] <- new_col_name
+    }
+  }
+  return(df_to_rename)
+}
+
+
+# Remove columns from a data frame.
 remove_columns <- function(df_with_all_columns) {
   cols_to_remove <- c("Original.name", "Sort.order")
   df_with_less_columns <- df_with_all_columns[, 
@@ -17,21 +38,11 @@ remove_columns <- function(df_with_all_columns) {
 }
 
 
-################################################################################
+###############################################################################
 # Main script
-data_path <- "./data/raw"
-all_files <- list.files(data_path, pattern = "*1.csv", full.names = TRUE)
-
-# Create a single dataframe from the CSV files
-dataframes <- list()
-for (filename in all_files) {
-  df <- read.csv(filename)
-  dataframes <- append(dataframes, list(df))
-}
-merged_df <- do.call(rbind, dataframes)
-
-# Tidy up the columns.
+merged_df <- merge_csv_files("./data/raw")
 merged_df <- rename_columns(merged_df)
 merged_df <- remove_columns(merged_df)
 
+# Save the transformed data frame.
 write.csv(merged_df, file = "./data/processed/VC40.csv", row.names = FALSE)
--- src_r/tests/test_sedn2brc.R
+++ src_r/tests/test_sedn2brc.R
@@ -4,19 +4,31 @@ source(here::here("src_r/sedn2brc.R"))
 
 # Define a test for the rename_columns function
 test_that("rename_columns renames columns correctly", {
-  # Create a test dataframe
+  # ARRANGE
   test_df <- data.frame(old_name1 = 1:3,
-                         old_name2 = c("a", "b", "c"),
-                         old_name3 = c(TRUE, FALSE, TRUE))
+                        old_name2 = c("a", "b", "c"),
+                        old_name3 = c(TRUE, FALSE, TRUE))
 
-  # Expected result
   expected_df <- data.frame(new_name1 = 1:3,
-                             new_name2 = c("a", "b", "c"),
-                             new_name3 = c(TRUE, FALSE, TRUE))
+                            new_name2 = c("a", "b", "c"),
+                            new_name3 = c(TRUE, FALSE, TRUE))
 
-  # Call the function
+  # ACT
   result_df <- rename_columns(test_df)
-
-  # Test that the function returns the expected result
+  # ASSERT
   expect_equal(result_df, expected_df)
+})
+
+
+test_that("remove_columns removes the correct columns", {
+  # ARRANGE
+  df <- data.frame(Original.name = c("John", "Jane"), Age = c(25, 30), 
+                   Sort.order = c(2, 1), Gender = c("Male", "Female"))
+  # ACT
+  result <- remove_columns(df)
+  # ASSERT
+  expect_equal(ncol(result), 2)
+  # Check that the resulting data frame does not contain the old columns
+  expect_false("Original.name" %in% colnames(result))
+  expect_false("Sort.order" %in% colnames(result))
 })
